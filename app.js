a7124ed85a1a328f199a1b69951b07e97337e442
simpleshow <simpleshow@Anikas-iMac.local>
2019-01-18 18:01:22 -0500

    added dotenv and additional oauth dependencies, started oauth process, used postman to send request and verify token, laid out additional app structure

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const path = require('path');
+const dotenv = require('dotenv')
+const result = dotenv.config()
 
-//init app
+if (result.error) {
+    throw result.error
+}
+
+//init app 
 const app = express();
+
+//init oauth
+// const OAuthServer = require("express-oauth-server");
+// const oauth = new OAuthServer({
+//     model: Model
+// });
+
+// app.get("/oauth/authorize", function (req, res) {
+//        res.render('index', {
+//            title: 'Hello',
+//            users: users
+//        });
+// });
+// app.post("/oauth/authorize", oauth.authorize());
+// app.post("/oauth/token", oauth.token());
+
+// https: //api.pinterest.com/oauth/?
+//     response_type = code &
+//     redirect_uri = https: //mywebsite.com/connect/pinterest/&
+//     client_id = 12345 &
+//     scope = read_public, write_public &
+//     state = 768 uyFys
 
 //views
 app.set('views', path.join(__dirname, 'views'));
